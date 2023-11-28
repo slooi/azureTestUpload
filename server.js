b1dfcb55137601e4fd067de66b171274896a837b
Samuel <s.looi2@outlook.com>
2023-11-29 00:17:54 +1300

    hi

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const app = require("express")()
-const PORT = process.env.PORT || 8080 ;
+const app = require("express")();
+const PORT = process.env.PORT || 8080;
 
-app.get("/",(req,res)=>{
-	res.send("<h1>Hello :)</h1>")
-})
-app.get("/hi",(req,res)=>{
-	res.send("<h1>Hello :)</h1>")
-})
+app.get("/", (req, res) => {
+	res.send("<h1>Hello :)</h1>");
+});
+app.get("/hi", (req, res) => {
+	res.send("<h1>hi :)</h1>");
+});
 
-app.listen(PORT,()=>console.log("Listening port "+PORT))
+app.listen(PORT, () => console.log("Listening port " + PORT));
