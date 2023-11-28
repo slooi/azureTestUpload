09c5d52417c8bc83405aa44542b8968a9800945f
s <s.looi2@outlook.com>
2023-11-28 21:44:32 +1300

    create a serverjs file

---
+++ server.js
@@ -0,0 +1,11 @@
+const app = require("express")()
+const PORT = process.env.PORT || 8080 ;
+
+app.get("/",(req,res)=>{
+	res.send("<h1>Hello :)</h1>")
+})
+app.get("/hi",(req,res)=>{
+	res.send("<h1>Hello :)</h1>")
+})
+
+app.listen(PORT,()=>console.log("Listening port "+PORT))
